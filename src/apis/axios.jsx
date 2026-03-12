2fa285b1336369042199ce18bc6fb62d61f27bfd
abeni-hub <ebenezarsileshi@gmail.com>
2026-03-12 21:57:29 +0300

    add the hosted url

--- src/apis/axios.jsx
+++ src/apis/axios.jsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://selam-b.naatipharmacy.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
